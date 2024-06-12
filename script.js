6334c7f8692e2bcd6c6ad8dfc2a6f4e89e4ac063
Lorenzo <lorenzzo.acquesta@gmail.com>
2024-06-11 22:56:25 -0300

    Inserindo arquivos
    Inserindo html básico, elementos no javascript e o arquivo do css, por enquanto vazio.

---
+++ script.js
@@ -0,0 +1,19 @@
+let usuarios = [
+    {
+        nome: 'Teste 1',
+        email: 'teste1@email.com',
+        senha: '123456',
+        tipo: 'admin',
+        moedas: 500,
+        km: 10,
+        nivel: 1,
+        interesses: ['Eletricidade', 'Quiz', 'Corrida', 'Campeonato', 'Formula1']
+    }
+]
+
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const form = document.querySelector('#form')
+
+// Indentificação do Ususario
+
