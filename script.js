0964c0d33427606bdf13621dc60481b4906ad91f
Lorenzo <lorenzzo.acquesta@gmail.com>
2024-06-16 11:59:18 -0300

    Inserindo css e validação login
    Nessa atualização foi adicionado ao site uma estilização simples e um login para os usuários já cadastrados

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 let usuarios = [
     {
         nome: 'Teste 1',
-        email: 'teste1@email.com',
+        email: 'teste1@gmail.com',
         senha: '123456',
         tipo: 'admin',
         moedas: 500,
         km: 10,
         nivel: 1,
         interesses: ['Eletricidade', 'Quiz', 'Corrida', 'Campeonato', 'Formula1']
+    },
+    {
+        nome: 'Teste 2',
+        email: 'teste2@gmail.com',
+        senha: '123456',
+        tipo: 'admin',
+        moedas: 250,
+        km: 5,
+        nivel: 1,
+        interesses: ['Esportes', 'Corrida', 'Campeonato', 'Competição']
     }
 ]
 
 const email = document.querySelector('#email')
-const password = document.querySelector('#password')
-const form = document.querySelector('#form')
+const password = document.querySelector('#senha')
+const button = document.querySelector('#button')
 
 // Indentificação do Ususario
+button.addEventListener('click', () => {
+    const usuario = usuarios.find(usuario => {
+        return usuario.email === email.value && usuario.senha === senha.value
+    })
+    if(usuario){
+        alert(`Bem-vindo ${usuario.nome}!`)
+    }else{
+        alert('Email ou senha incorretos')
+    }
+    console.log(usuario);
+})
 
+// let usuarioEncontrado = usuarios.find((usuario) => {
+//     if(usuario.email === email.value && usuario.senha === password.value){
+//         window.location.href= "./areaDoUsuario.html"
+//     }
+//     else{
+//         alert("Email ou senha inválidos")
+//     }
+// })
